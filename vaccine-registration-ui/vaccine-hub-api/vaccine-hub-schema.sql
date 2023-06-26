2dd061c065b2cf40853386b7b3550ae1d8911190
oyindamola <oyindamola.akanbi@oyindam-ltmujtm.internal.salesforce.com>
2023-06-26 15:47:49 -0700

    added the db and scripts

---
+++ vaccine-registration-ui/vaccine-hub-api/vaccine-hub-schema.sql
@@ -0,0 +1,13 @@
+
+
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    location VARCHAR(255) NOT NULL,
+    date DATE NOT NULL
+    
+);
